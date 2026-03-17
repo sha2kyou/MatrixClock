9674939c1d1150be457f401e6a0fc6165922c9c3
SHARK YOU <416561115@qq.com>
2026-03-17 18:27:17 +0800

    chore(build): update versionCode and versionName in build.gradle.kts
    Increment versionCode to 10000 and set versionName to "1.0.0" for the upcoming release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cn.tr1ck.matrixclock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10000
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
